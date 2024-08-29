fe0260671c8dfbe2ec852a886a1a801f866e802a
Tamaes <125809997+dasaraul@users.noreply.github.com>
2024-08-29 07:22:14 +0000

    v

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 sudo apt update -y
 sudo apt upgrade -y
 python3 -m pip install --upgrade pip
+sudo pip3 install yt-dlp==2023.10.13
 sudo docker build . -t tamaesmirror && sudo docker run -p 80:80 -p 8080:8080 tamaesmirror
